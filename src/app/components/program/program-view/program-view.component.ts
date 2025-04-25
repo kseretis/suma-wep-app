3872c5966df3e0e8a6cabf6b86280a4e564f2bf7
kseretis <serkleanthis@gmail.com>
2025-04-25 16:57:12 +0300

    Response model created, http service fixes, add-program code moved to program-view

--- src/app/components/program/program-view/program-view.component.ts
+++ src/app/components/program/program-view/program-view.component.ts
@@ -1,21 +1,46 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
-import { Component, OnInit } from '@angular/core';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { ProgramService } from '../../../services/program.service';
 import { Program } from '../../../models/program';
-import { DatePipe } from '@angular/common';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-program-view',
-  imports: [NzTableModule, NzButtonModule, DatePipe, NzCheckboxModule ],
+  imports: [
+    NzTableModule,
+    NzButtonModule,
+    DatePipe,
+    NzCheckboxModule,
+    NzModalModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzSelectModule,
+    NzCardModule,
+    NzInputModule,
+  ],
   templateUrl: './program-view.component.html',
   styleUrl: './program-view.component.scss',
 })
 export class ProgramViewComponent implements OnInit {
   programs: Program[] = [];
   isLoading: boolean = false;
+  isAddProgramModalVisible: boolean = false;
+
+  private fb = inject(NonNullableFormBuilder);
+  validateForm = this.fb.group({
+    name: this.fb.control('', [Validators.required]),
+    isActive: this.fb.control(true, [Validators.required]),
+  });
 
   constructor(private programService: ProgramService) {}
 
@@ -27,7 +52,7 @@ export class ProgramViewComponent implements OnInit {
     this.isLoading = true;
     this.programService.getPrograms().subscribe({
       next: (response) => {
-        this.programs = response;
+        this.programs = response.data as Program[];
         console.log('Programs:', this.programs);
       },
       error: (error) => {
@@ -39,8 +64,26 @@ export class ProgramViewComponent implements OnInit {
     });
   }
 
-  addProgram(): void {
-    alert('To be added!');
-    // TODO open modal to add program
+  createProgram(): void {
+    if (this.validateForm.invalid) {
+      return;
+    }
+
+    this.programService
+      .createProgram({
+        name: this.validateForm.value.name,
+        isActive: this.validateForm.value.isActive,
+      })
+      .subscribe({
+        next: (response) => {
+          console.log('return: ', response.message);
+        },
+        error: (error) => {
+          console.error('Error creating program:', error);
+        },
+        complete: () => {
+          this.loadPrograms();
+        },
+      });
   }
 }
--- src/app/services/program.service.ts
+++ src/app/services/program.service.ts
@@ -1,33 +1,31 @@
-import { HttpClient, } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, take, catchError } from 'rxjs';
+import { Observable, take, catchError, map } from 'rxjs';
 import { Program } from '../models/program';
 import { ennvironment } from '../../environment';
+import { Response } from '../models/response';
+
+const PROGRAM_ENDPOINT = '/Program';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProgramService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  createProgram(program: Program): void {
-    // return this.http.get<Program[]>(`${ennvironment.apiUrl}/Program`)
-    // .pipe(
-    //   take(1)
-    // );
-
-
-      //   (response) => {
-      //   console.log('Program created:', response);
-      // });
-  }
-
-  getPrograms(): Observable<Program[]> {
-    return this.http.get<Program[]>(`${ennvironment.apiUrl}/Program`)
+  createProgram(program: Program): Observable<Response> {
+    return this.http
+      .post<Response>(`${ennvironment.apiUrl}${PROGRAM_ENDPOINT}`, program)
       .pipe(
         take(1)
       );
   }
 
+  getPrograms(): Observable<Response> {
+    return this.http
+      .get<Response>(`${ennvironment.apiUrl}${PROGRAM_ENDPOINT}`)
+      .pipe(
+        take(1)
+      );
+  }
 }
